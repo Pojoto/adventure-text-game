d5026a77dd6ea987a5a60bef3c2746e4befb52ee
Pojoto <114115897+Pojoto@users.noreply.github.com>
2023-04-27 22:30:39 -0400

    added subclass support for items, reading file functions, separate item handlers

--- src/items.py
+++ src/items.py
@@ -1,18 +1,19 @@
+import os
 
+#names of all the files in the object_data directory 
+filenames = ["normal_items_data.txt", "containers_data.txt", "foods_data.txt"]
 
 '''
 The Item class represents a single item in the game.
 '''
 class Item:
     
-    def __init__(self, name, full_name, weight, capacity, status, description, contents):
+    def __init__(self, name, full_name, weight, status, description):
         self.name = name  # a one-word name that serves as a unique identifier
         self.full_name = full_name  # includes adjective
         self.weight = weight  # indicates weight or size
-        self.capacity = capacity  # a capacity greater than 0 is a container
         self.status = status  # a word that describes the status of this item
         self.description = description  # describs the item when examined
-        self.contents = contents  # a list of item names held by this item
     
     def __str__(self):
         result = ''
@@ -26,62 +27,95 @@ class Item:
                 count += len(word) + 1
         return result
 
-
 class Food(Item):
 
-    def __init__(self, name, full_name, weight, capacity, status, description, contents, hp_amount):
-        super().__init__(name, full_name, weight, capacity, status, description, contents)
+    def __init__(self, name, full_name, weight, status, description, hp_amount):
+        super().__init__(name, full_name, weight, status, description)
         self.hp_amount = hp_amount
 
 
 class Container(Item):
-    pass
-    #CONTINUE
 
+    def __init__(self, name, full_name, weight, status, description, capacity, contents):
+        super().__init__(name, full_name, weight, status, description)
+        self.capacity = capacity  
+        self.contents = contents  # a list of item names held by this item
+
+#TODOO -- make funciton that can read normal itme files and put it in all below 3 function
+
+def read_foods(objects_dict, foods_file_name):
+
+    with open(foods_file_name, "r") as foods_file:
+        name = foods_file.readline().strip()
+        while name != '':
+            full_name = foods_file.readline().strip()
+            weight = int(foods_file.readline().strip())
+            status = foods_file.readline().strip()
+            description = foods_file.readline().strip()
+            hp_amount = int(foods_file.readline().strip())
+
+            objects_dict[name] = Food(name, full_name, weight,
+                                        status, description, hp_amount)
+            foods_file.readline()
+            name = foods_file.readline().strip()
+
+def read_containers(objects_dict, containers_file_name):
+
+    with open(containers_file_name, "r") as containers_file:
+        name = containers_file.readline().strip()
+        while name != '':
+            full_name = containers_file.readline().strip()
+            weight = int(containers_file.readline().strip())
+            status = containers_file.readline().strip()
+            description = containers_file.readline().strip()
+            capacity = int(containers_file.readline().strip())
+            contents = containers_file.readline().strip()
+            if contents == "nothing":
+                contents = []
+
+            objects_dict[name] = Container(name, full_name, weight,
+                                        status, description, capacity, contents)
+            containers_file.readline()
+            name = containers_file.readline().strip()
+            
+def read_normal_items(objects_dict, normal_file_name):
+    
+    with open(normal_file_name, "r") as normal_items_file:
+
+        name = normal_items_file.readline().strip()
+
+        while name != '':
+            full_name = normal_items_file.readline().strip()
+            weight = int(normal_items_file.readline().strip())
+            status = normal_items_file.readline().strip()
+            description = normal_items_file.readline().strip()
+
+            objects_dict[name] = Item(name, full_name, weight,
+                                        status, description)
+            normal_items_file.readline()
+            name = normal_items_file.readline().strip()
+            
 '''
 The Items class is a collection of all items in the game.
 '''
 class Items:
     
-    def __init__(self, items_file_name):
+    def __init__(self, objects_directory):
         
-        self.items_dict = {}  # maps an item name to the corresponding Item object
+        self.objects_dict = {}  # maps an item name to the corresponding Item object
+
+        path = objects_directory + filenames[0]
+        read_normal_items(self.objects_dict, path)
+
+        path = objects_directory + filenames[1]
+        read_containers(self.objects_dict, path)
         
-        items_file = open(items_file_name, 'r')
+        path = objects_directory + filenames[2]
+        read_foods(self.objects_dict, path)
+
         
-        name = items_file.readline().strip()
-        
-        while name != '':
-            isFood = False
-            full_name = items_file.readline().strip()
-            if "food" in full_name:
-                isFood = True
-            weight = int(items_file.readline().strip())
-            capacity = int(items_file.readline().strip())
-            status = items_file.readline().strip()
-            description = items_file.readline().strip()
-            if capacity == 0:
-                contents = []
-            else:
-                contents = items_file.readline().split()
-                if contents[0] == 'nothing':
-                    contents = []
-            
-            if isFood:
-                hp = int(items_file.readline().strip())
-                self.items_dict[name] = Food(name, full_name, weight, capacity,
-                                             status, description, contents, hp)
-            else:
-                self.items_dict[name] = Item(name, full_name, weight, capacity,
-                                         status, description, contents)
-            
-            items_file.readline()  # consume blank line between item entries
-            name = items_file.readline().strip()
-    
-    
     def __str__(self):
         return str(self.items_dict)
-    
     
     def get_item(self, item_name):
         return self.items_dict[item_name]
@@ -92,14 +126,13 @@ class Items:
             if isinstance(item, Food):
                 foods.add(item.name)
         return foods
-    
+
     def get_containers(self):
         containers = set()
         for item in self.items_dict.values:
             if isinstance(item, Container):
                 containers.add(item)
         return containers
-
 
     def get_total_weight(self, items):
         total = 0
